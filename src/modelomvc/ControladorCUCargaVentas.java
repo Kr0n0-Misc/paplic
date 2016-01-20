f2874f68826c5737ca0905a52afff6f02479a66a
Carlos Crisóstomo Vals <ccvals@linux.com>
2016-01-20 14:12:45 +0100

    Añadidas referencias a Cliente, Articulo y Venta en el CU de CargaVentas para poder referenciar la existencia

--- src/modelomvc/ControladorCUCargaVentas.java
+++ src/modelomvc/ControladorCUCargaVentas.java
@@ -5,18 +5,25 @@
  */
 package modelomvc;
 
+// Contenedores
 import contenedores.ContenedorArticulosSingleton;
 import contenedores.ContenedorClientesSingleton;
 import contenedores.ContenedorTiendasSingleton;
 import contenedores.ContenedorVentasSingleton;
+// Entidades
 import entidades.Venta;
 import entidades.DatoFichero;
 import entidades.RegistroFichero;
+import entidades.Articulo;
+import entidades.Cliente;
+import entidades.Tienda;
+// Gestión de carga
+import proveeedores.ProveedorVentas;
+import validacionesdatos.ValidarVenta;
+// Generales
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.LinkedList;
-import proveeedores.ProveedorVentas;
-import validacionesdatos.ValidarVenta;
         
 /**
  *
@@ -83,10 +90,31 @@ public class ControladorCUCargaVentas {
             
             // VALIDACION DE DATOS
             if (validador.esValido(venta)) {
+                // Comprobamos que exista el cliente, producto y tienda buscando en los contenedores existentes.
+
+                // Cliente
+                Cliente cliente = ContenedorClientesSingleton.getInstance().getCliente(venta.getsIDCliente());
+                if (cliente == null)
+                        System.out.println("Cliente "+venta.getsIDCliente()+" no encontrado en el sistema.");
+                // Articulo
+                Articulo articulo = ContenedorArticulosSingleton.getInstance().getArticulo(venta.getsIDArticulo());
+                if (articulo == null)
+                        System.out.println("Articulo "+venta.getsIDArticulo()+" no encontrado en el sistema.");
+                // Tienda
+                Tienda tienda = ContenedorTiendasSingleton.getInstance().getTienda(venta.getsIDTienda());
+                if (tienda == null)
+                        System.out.println("Tienda "+venta.getsIDTienda()+" no encontrada en el sistema.");
+                                
+                if ((cliente != null) && (articulo != null) && (tienda != null)) {
+                    //Guardamos las referencias a las entidades
+                    venta.setaArticulo(articulo);
+                    venta.setcCliente(cliente);
+                    venta.settTienda(tienda);
                     //Guardamos en el contenedor
                     contenedorVen.addVenta(venta);
                     // Mostramos mensaje de almacenar
                     System.out.println("CargarVentas - "+venta.getsIDVenta()+" venta validada y guardada.");
+                }
             }
            
         } // FIN BUCLE REGISTROS
